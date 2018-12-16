b3e20d02b9ff8b395707145229ff561a8f2f3344
Tim Gallagher <timothy1111g@yahoo.com>
2018-12-16 12:43:39 -0800

    updated program and define for functions to work

--- test.cpp
+++ test.cpp
@@ -4,13 +4,14 @@
 #include "Object.h"
 using namespace std;
 
-int main ()
-{
-	cout << Object(12) + Object(5);
+int main(	cout << 	12 + 5;
 	cout << endl;
-	cout << Object("abc") + Object("xyz");
+;
+	cout << 	"abc" + "xyz";
 	cout << endl;
-	cout << Object("abc") + Object(5);
+;
+	cout << 	"abc" + 5;
 	cout << endl;
+}
 	return 0;
 }
